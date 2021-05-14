3ea60d77e0c6b417cf55101a421027838d789846
TheoB-png <theo.beloeil.pro@gmail.com>
2021-05-14 17:58:43 +0200

    modif port

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ app.use(express.json());
 
 app.use(router);
 
-app.listen(process.env.PORT, () => console.log('listening on 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('listening on 3000'));
