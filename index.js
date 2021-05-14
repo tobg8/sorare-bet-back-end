b04b1e2118d3a9bd2c2160e72dc5e7118afb1470
TheoB-png <theo.beloeil.pro@gmail.com>
2021-05-14 17:46:10 +0200

    test3

--- index.js
+++ index.js
@@ -1,117 +1,16 @@
-const axios = require('axios');
-const bcrypt = require('bcrypt');
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const router = require('./app/router');
+const port = '3000';
 
-const fetchTest = async () => {
-    const email = 'sorarebusiness@yahoo.com'
-    const response = await axios.get(`https://api.sorare.com/api/v1/users/${email}`);
-    
-    // Need to retrieve response.data.salt
-    const salt = response.data.salt;
-    const password = 'theomula987!';
 
-    // Create hashed password
-    const saltyPassword = bcrypt.hashSync(password, salt);
-    console.log(saltyPassword)
+app.use(cors({
+    origin: 'http://localhost:8080'
+}));
+app.use(express.json());
 
-    // Need to retrieve _sorare_session_id
-    const cookie = response.headers['set-cookie'][1];
+app.use(router);
 
-    // Need to retrieve a X-CSRF-TOKEN
-    const CSRF = response.headers['csrf-token'];
-
-    // Create Headers for post
-    // const headers = {
-    //     "x-csrf-token": CSRF,
-    //     "set-cookie": cookie,
-    //     'Content-Type': 'application/json'
-    // }
-
-   // Prepare Query and infos
-    const url = 'https://api.sorare.com/graphql';
-    const body = {
-        query: `
-            mutation SignInMutation($input: signInInput!) {
-                signIn(input: $input) {
-                currentUser {
-                    slug
-                    cardsCount
-                }
-                otpSessionChallenge
-                errors {
-                    message
-                    code 
-                    path
-                }
-                } 
-            }
-        `,
-        variables: {
-            input: {
-                "email": email,
-                "password": saltyPassword,
-            }
-        }
-    };
-    
-    const test = await axios.post(url, body)
-    // console.log(test.data.data.signIn.otpSessionChallenge)
-    const otpChall = test.data.data.signIn.otpSessionChallenge;
-    console.log(otpChall)
-    const otpCode = '226831'
-    const body2 = {
-        query: `
-            mutation SignInMutation($input: signInInput!) {
-                signIn(input: $input) {
-                currentUser {
-                    slug
-                    cardsCount
-                    cards {
-                        slug
-                        age
-                        grade
-                        name
-                        id
-                        rarity
-                        pictureUrl
-                        player {
-                            so5Scores(last:5) {
-                                score
-                                player {
-                                    slug
-                                }
-                                detailedScore {
-                                    totalScore
-                                }
-                            }
-                        }
-                    }
-                }
-                errors {
-                    message
-                    code 
-                    path
-                }
-                }
-            }
-        `,
-        variables: {
-            input: {
-                "otpSessionChallenge": otpChall,
-                "otpAttempt": otpCode,
-            },
-            last: "5",
-        }
-    };
-    const finalTest = await axios.post(url, body2);
-    // if(finalTest.data) {
-    //     console.log(finalTest.data.errors);
-    // }
-    console.log(finalTest.data.data.signIn.currentUser.cards[0].player.so5Scores);
-    // if(finalTest.data.data.currentUser) {
-    //     console.log(finalTest.data.data.signIn.currentUser);
-    // }
-    
-    
-}
-
-fetchTest();
+app.listen(port, () => console.log('listening on 3000'));
