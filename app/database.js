4e49f8af0bfb5ded6e79f29105d336a8dd2cf657
TheoB-png <theo.beloeil.pro@gmail.com>
2021-05-17 23:54:24 +0200

    fix psql connection

--- app/database.js
+++ app/database.js
@@ -7,9 +7,16 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
         timestamps: false,
         underscored: true,
     },
-    ssl: {
-        rejectUnauthorized: false
-    }
+    dialect: 'postgres',
+    ssl: true,
+    protocol: "postgres",
+    logging: true,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false //
+        }
+    },
 });
 
 module.exports = sequelize;
