dd843f8d69cb0cb2c6fd4aeec2b67803d0639763
TheoB-png <theo.beloeil.pro@gmail.com>
2021-05-22 20:24:37 +0200

    azeaz

--- schedules.js
+++ schedules.js
@@ -4,14 +4,32 @@ const { League } = require ('./app/models');
 
 module.exports.cron_job = async () =>
 {       
+        const url = process.env.API_URL;
         // Fetch current Leagues
-        const leagues = await gamesController.getLeagues();
+        const fetchLeagues = await axios({
+                url: url,
+                method: 'post',
+                data: {
+                        query: `
+                        {
+                        featuredSo5Fixtures(first: 3) {
+                                aasmState
+                                gameWeek
+                                slug
+                                canCompose
+                                startDate
+                                endDate
+                        }
+                        }
+                        `        
+                },
+        });
+
+        console.log(fetchLeagues);
         // Pour chacune des league reçues je vais voir si elle existe dans ma bdd,
                 // si elle est nouvelle je l'insère en bdd je la met à open etc
                 // Si elle est pas nouvelle et qu'elle est passée de opened à started on modifie les propriété nécessaires
                 // SI elle est passée de started à closed on fait les scores on modifie les données.
-
-        console.log(leagues);
 }
 
 require('make-runnable');
