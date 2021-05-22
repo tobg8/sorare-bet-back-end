9a227002898cb060fed9b0236430d73e31a343dc
TheoB-png <theo.beloeil.pro@gmail.com>
2021-05-22 20:18:13 +0200

    test cron

--- schedules.js
+++ schedules.js
@@ -1,6 +1,17 @@
-module.exports.cron_job = function()
-{
-        console.log('its working');
+const axios = require('axios');
+const gamesController = require('./controllers/gamesController');
+const { League } = require ('../models');
+
+module.exports.cron_job = async () =>
+{       
+        // Fetch current Leagues
+        const leagues = await gamesController.getLeagues();
+        // Pour chacune des league reçues je vais voir si elle existe dans ma bdd,
+                // si elle est nouvelle je l'insère en bdd je la met à open etc
+                // Si elle est pas nouvelle et qu'elle est passée de opened à started on modifie les propriété nécessaires
+                // SI elle est passée de started à closed on fait les scores on modifie les données.
+
+        console.log(leagues);
 }
 
 require('make-runnable');
