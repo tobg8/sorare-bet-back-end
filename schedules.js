a0977b6e36f2458d724cb8a440ed8f213f7ba305
TheoB-png <theo.beloeil.pro@gmail.com>
2021-05-23 18:56:41 +0200

    aeazea

--- schedules.js
+++ schedules.js
@@ -67,11 +67,12 @@ module.exports.cron_job = async () =>
                                                         // we create an array with slugs of card
                                                         const slugsArray = [];
                                                         registration.dataValues.cards.map( async (card) => {
-                                                                let slugModif = card.slug.replace(/'/g,'v');
-                                                                slugsArray.push(slugModif);
+                                                                slugsArray.push(card.slug);
                                                         });
                                                         console.log(slugsArray);
                                                         console.log("coool");
+                                                        slugsArray.map((slug) => slug.replace(/'/g,'"'));
+                                                        console.log(slugsArray);
                                                         // then we query last score of cards using our slugsArray
                                                         const fetchScores = await axios({
                                                                 url: url,
