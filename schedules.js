51607a63f5af3983d30559cedd2b108f342de597
TheoB-png <theo.beloeil.pro@gmail.com>
2021-05-23 19:47:42 +0200

    aeaze

--- schedules.js
+++ schedules.js
@@ -92,6 +92,7 @@ module.exports.cron_job = async () =>
                                                         const teamWithScore = []
                                                         fetchScores.data.data.cards.map((player) => {
                                                                 if (player.player.so5Scores[0].score === null) {
+                                                                        console.log('null here');
                                                                         teamWithScore.push({
                                                                                 slug: player.slug,
                                                                                 score: 0,
