20b0a476fbdeb2d7dccfaf6e6497f9b2cb7b95d8
TheoB-png <theo.beloeil.pro@gmail.com>
2021-05-23 18:34:26 +0200

    aeazea

--- schedules.js
+++ schedules.js
@@ -84,14 +84,20 @@ module.exports.cron_job = async () =>
                                                                 method:'post',
                                                                 data: {
                                                                         query:`{
-                                                                                cards(slugs:${testSlugs}) {
-                                                                                        player {
-                                                                                                so5Scores(last: 1) {
-                                                                                                        score
-                                                                                                }
-                                                                                        }
+                                                                                cards(slugs:[
+                                                                                "yuta-matsumura-2020-rare-38",
+                                                                                "thibaut-courtois-2020-common-b53109ab-0dd1-4400-a982-2cbf10723312",
+                                                                                "laurtaro-giaccone-2020-rare-2",
+                                                                                "fabrice-olinga-essono-2020-rare-25",
+                                                                                "david-ospina-ramirez-2020-common-973982a6-755c-453b-81f1-ece786217a6b"
+                                                                              ]) {
+                                                                                  player {
+                                                                                    so5Scores(last: 1) {
+                                                                                      score
+                                                                                    }
+                                                                                  }
                                                                                 }
-                                                                        }
+                                                                              }
                                                                         `
                                                                 },
                                                         });
